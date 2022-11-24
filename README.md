d6382b5d90aa6fea8a9f1787f6e64249a5b876fc
priyanshshah23 <shahpriyansh91>
2022-11-24 22:25:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Task 2
+
+In this project i have implemented
+- GridView with 2 column
+- Showing random images loaded from API to local database
+- Pagination for loading new sets of images and 10 images are shown per page
+
+### Output
+
+- <h4>Showing images loaded from given url to the local database</h4>
+- <h4>Pagination done when scrolling then next 10 images will be loaded</h4>
+- <h4>Showing Chached Images loaded from url</h4>
+- <h4>New set of 10 cached images on scrolling </h4><br>
---
+++ lib/secondpage.dart
@@ -0,0 +1,51 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'Images.dart';
+
+class secondPage extends StatefulWidget {
+  const secondPage(this.images);   //initialized constructor with the necessary parameter
+  
+  @required
+  final Images images;   //creating object of class image
+
+  @override
+  _secondPageState createState() => _secondPageState();
+}
+
+class _secondPageState extends State<secondPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(          //implementing card for each data
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Flexible(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: CachedNetworkImage(  //used cached network image to display the already loaded random images
+                      width: 200,
+                      height: 200,
+                      placeholder: (context,url)=>CircularProgressIndicator(), //progess bar uptill the images doesn't load
+                      imageUrl: widget.images.image,   //showing the data which are stored in local database
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+}
+
+
